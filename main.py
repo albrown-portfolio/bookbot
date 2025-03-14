eb946c59769194d5d64d8a5ea40975d72a1cfa1c
albrown-portfolio <albrown89@gmail.com>
2025-03-14 15:58:06 -0600

    basic word count

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from stats import count_words
+
+def main ():
+    book_path="books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    print(f"{num_words} words found in the document")
 
 def get_book_text(path):
     with open(path) as book:
         return (book.read())
 
-def main ():
-    r_path="books/frankenstein.txt"
-    print(get_book_text(r_path))
-    
-if __name__ == "__main__":
-    main()
+
+main()
