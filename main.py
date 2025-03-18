7070886eb1e09f0fc7b0b930832a36fb67f44b7a
albrown-portfolio <albrown89@gmail.com>
2025-03-18 11:07:42 -0600

    report finished

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def get_book_text(path):
     with open(path) as book:
         return (book.read())
 
+def print_counts(dict_list):
+    for item in dict_list:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['count']}")
+
 def main ():
     book_path="books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -17,11 +22,6 @@ def main ():
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    #print alphanumeric only
-    
-    print(f"{dict_list})")
+    print_counts(dict_list)
     print("============= END ===============")
-
-
-
 main()
